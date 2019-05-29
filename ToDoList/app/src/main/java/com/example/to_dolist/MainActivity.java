9a82a6229de3d05b6fa726b303ff883a28f82ad2
Kelly <chengkejia@gmail,com>
2019-05-29 02:40:27 -0700

    login page

---
+++ ToDoList/app/src/main/java/com/example/to_dolist/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.to_dolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.to_dolist.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ ToDoList/app/src/main/java/com/example/to_dolist/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.example.to_dolist.ui.main;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
